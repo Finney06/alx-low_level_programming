c751c94aa72252971d9ff26597619b20a77dcffa
Osajere Finney <osafinney21@gmail.com>
2022-10-19 22:12:37 +0000

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of lon long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	return (0);
 }
