d33001a84fbb145b0b7225f2d7f22bec19d4ec4f
Osajere Finney <osafinney21@gmail.com>
2022-10-19 22:03:11 +0000

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+*       main - Entry point
+*      Description: 'to print  prints the size of various types on the computer'
+*       Return: Always 0 (Success)
+**/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(a));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of lon long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	return (0);
+}
