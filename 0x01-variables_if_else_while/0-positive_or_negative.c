68541d446bf706a482aa5da41d361fc7069d6c31
finney <osafinney21@gmail.com>
2022-10-25 20:08:30 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *	main - Entry Point
+ *	Description: Task 0
+ *	Return: Always 0 (success)
+ **/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand(0 - RAND_MAX / 2;
+	if (n
