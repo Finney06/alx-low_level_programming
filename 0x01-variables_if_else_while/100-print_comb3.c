0b9c2eeed90d4400e417979139177f5882230991
Osajere Finney <osafinney21@gmail.com>
2022-10-27 13:37:23 +0000

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,18 @@ int main(void)
 	int i;
 	int j;
 
-	for ( i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 {
-		for ( j = i + 1; j  < 10; j++)
+	for (j = i + 1; j  < 10; j++)
 {
-	putchar((i % 10) + '0');
-	putchar((j % 10) + '0');
+	putchar ((i % 10) + '0');
+	putchar ((j % 10) + '0');
 		if (i == 8 && j == 9)
 			continue;
-	putchar(',');
-	putchar(' ');
+	putchar (',');
+	putchar (' ');
 }
 }
-	putchar('\n');
+	putchar ('\n');
 	return (0);
 }
