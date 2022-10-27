d69205a5e2e75c3b4a54e04e71338bae329bec78
Osajere Finney <osafinney21@gmail.com>
2022-10-27 00:25:53 +0000

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*	main - Entry Point
+*	Description: 'Task 8'
+*	Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+{
+	putchar ((i % 10) + '0');
+}
+	for (i = 'a'; i <= 'f'; i++)
+{
+	putchar (i);
+}
+	putchar ('\n');
+	return (0);
+}
