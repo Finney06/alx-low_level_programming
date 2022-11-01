da581f2973cf6ab0cb98e4e2313043dd8d178ede
Osajere Finney <osafinney21@gmail.com>
2022-11-01 21:24:43 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*	main - Entry Point
+*
+*	Return: Always 0 (Success)
+**/
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+{
+
+	putchar(letter);
+	letter++;
+}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/* _putchar.c */
-int _putchar(char c);
 
+/* _putchar.c */
+int _putchar(char);
+
+/* 1-alphabet.c */
+void print_alphabet(void);
 #endif
