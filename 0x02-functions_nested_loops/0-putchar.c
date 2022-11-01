804d2b4de297f4b028c6b86a7e4c605302d5dd18
Osajere Finney <osafinney21@gmail.com>
2022-11-01 11:04:25 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*	main - Entry Point
+*	Description: 'A program that prints _putchar'
+*	Return: Always 0 (Success)
+**/
+int main(void)
+{
+	printf("_putchar.c\n");
+	return (0);
+}
